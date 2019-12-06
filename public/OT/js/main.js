ca2bbb527da9dfadc86d22d957ae798d730d1d4f
Emilie Teytau <eteytau@emi.u-bordeaux1.fr>
2019-12-06 07:33:54 +0100

    Blabla

--- public/OT/js/main.js
+++ public/OT/js/main.js
@@ -1,3 +1,18 @@
 (()=> {
-  alert("oui O-T");
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: "Description de l'équipe ",
+      messagebis : "- C'est une bonne situation ça développeur ? ",
+      messagebisbis : "- Vous savez, moi je ne crois pas qu’il y ait de bon ou de mauvais code. Moi, si je devais résumer ma vie aujourd’hui avec vous, je dirais que c’est d’abord des rencontres. Des gens qui m’ont tendu la main, peut-être à un moment où je ne pouvais pas, où j’étais seul chez moi. Et c’est assez curieux de se dire que les hasards, les rencontres forgent un projet… Parce que quand on a le goût du code, quand on a le goût du code bien fait, le beau geste, parfois on ne trouve pas l’interlocuteur en face je dirais, le miroir qui vous aide à avancer. Alors cela n’est pas mon cas, comme je disais là, puisque moi au contraire, j’ai pu : et je dis merci à la vie, je lui dis merci, je chante la vie, je danse la vie, je code la vie… je ne suis que commentaire et syntaxe! Et finalement, quand beaucoup de gens aujourd’hui me disent « Mais comment fais-tu pour avoir cette configuration ? », et bien je leur réponds très simplement, je leur dis que c’est ce goût de topachat ce goût donc qui m’a poussé aujourd’hui à entreprendre une construction mécanique, mais demain qui sait ? Peut-être simplement à me mettre au service de la communauté, à faire le don, le don de soi ou de 20 balles au Zevent…",
+      noms : "Charles Goedefroit, Amandine Semeteys, Emilie Teytau, Quentin Désiré, Simon Feugeas, Arthur Blondeau , Victor Andrault, Roger Juvé Vallès et Sarah De Saleneuve."
+    }
+  })
+  var app2 = new Vue({
+    el: '#ap2',
+    data: {
+      desc2: "Description de notre projet",
+      projet : "Nous avons décidé d'aborder le thème de cette année, la précarité étudiante, en relevant 4 défis : le webGl, le gif ubisoft, leroy merlin et le site. D'un commun accord et sous l'impulsion de notre leader on a voulu les présenter au travers d'un site avec une interface qui mène vers construis ton projet, 3 mini-jeux et une animation 3D. Notre objectif à travers cette interface a été d'augmenter le pouvoir d'agir des étudiants en situation de précarité ",
+    }
+  })
 })();
