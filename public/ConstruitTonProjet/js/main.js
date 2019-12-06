56ca2dded85828e19be73201693cfe406bc4105c
Amandine Semeteys <amandine.semeteys@etu.u-bordeaux.fr>
2019-12-06 07:30:19 +0100

    construit ton projet fini

--- public/ConstruitTonProjet/js/main.js
+++ public/ConstruitTonProjet/js/main.js
@@ -1,9 +1,13 @@
-(()=> {
-  // var app = new Vue({
-  //   el: '#app',
-  //   data: {
-  //     message: 'Hello Vue!'
-  //   }
-  // })
+$(document).ready(function() {
+  $.getJSON("/ConstruitTonProjet/departementsLeroyMerlin.json", function (r) {
+    console.log(r);
+    
+    var app = new Vue({
+      el: '#app',
+      data: {
+        magasins: r
+      }
+    })
+  })
 
-})();
+});
