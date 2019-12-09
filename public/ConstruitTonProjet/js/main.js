ba28120021d336333a4eafe126ba4d81e0ab85b1
charles66820 <charles.goedefroit@gmail.com>
2019-12-09 12:46:30 +0100

    lol 5 minute en aitent réveiller

--- public/ConstruitTonProjet/js/main.js
+++ public/ConstruitTonProjet/js/main.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
   $.getJSON("/ConstruitTonProjet/departementsLeroyMerlin.json", function (r) {
-    console.log(r);
-    
+
     var app = new Vue({
       el: '#app',
       data: {
         magasins: r
       }
-    })
+    });
+    
+    $("#filter").on("keyup", function() {
+      let value = $(this).val().toLowerCase();
+      $("#app tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
   })
 
 });
